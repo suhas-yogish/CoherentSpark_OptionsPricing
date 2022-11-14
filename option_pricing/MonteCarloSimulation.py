f065a51fdb5b053383effd85acb9b87084fd1138
suhas-yogish <72915146+suhas-yogish@users.noreply.github.com>
2022-11-14 20:07:50 +0800

    Connected to Spark model

--- option_pricing/MonteCarloSimulation.py
+++ option_pricing/MonteCarloSimulation.py
@@ -1,6 +1,8 @@
 # Third party imports
+import requests
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.stats import norm 
+import json
 
 # Local package imports
 from .base import OptionPricingModel
@@ -37,55 +39,83 @@ class MonteCarloPricing(OptionPricingModel):
         self.num_of_steps = days_to_maturity
         self.dt = self.T / self.num_of_steps
 
-    def simulate_prices(self):
-        """
-        Simulating price movement of underlying prices using Brownian random process.
-        Saving random results.
-        """
-        np.random.seed(20)
-        self.simulation_results = None
-
-        # Initializing price movements for simulation: rows as time index and columns as different random price movements.
-        S = np.zeros((self.num_of_steps, self.N))        
-        # Starting value for all price movements is the current spot price
-        S[0] = self.S_0
-
-        for t in range(1, self.num_of_steps):
-            # Random values to simulate Brownian motion (Gaussian distibution)
-            Z = np.random.standard_normal(self.N)
-            # Updating prices for next point in time 
-            S[t] = S[t - 1] * np.exp((self.r - 0.5 * self.sigma ** 2) * self.dt + (self.sigma * np.sqrt(self.dt) * Z))
-
-        self.simulation_results_S = S
 
     def _calculate_call_option_price(self): 
         """
         Call option price calculation. Calculating payoffs for simulated prices at expiry date, summing up, averiging them and discounting.   
         Call option payoff (it's exercised only if the price at expiry date is higher than a strike price): max(S_t - K, 0)
         """
-        if self.simulation_results_S is None:
-            return -1
-        return np.exp(-self.r * self.T) * 1 / self.N * np.sum(np.maximum(self.simulation_results_S[-1] - self.K, 0))
-    
+
+        url = "https://excel.staging.coherent.global/coherent/api/v3/folders/Microsoft Envision/services/MonteCarloSimulation/Execute"
+
+        payload = json.dumps({
+          "request_data": {
+            "inputs": {
+              "daystoexpire": self.num_of_steps,
+              "numSimulations": self.N,
+              "historicvolatility": self.sigma,
+              "price": self.S_0,
+              "riskfreerate": self.r,
+              "strikeprice": self.K
+            }
+          },
+          "request_meta": {
+            "version_id": "4d5274e8-9b0d-49f6-873e-536537b237be",
+            "call_purpose": "Spark - API Tester",
+            "source_system": "SPARK",
+            "correlation_id": "",
+            "requested_output": None,
+            "service_category": "",
+            "compiler_type": "Type3"
+          }
+        })
+        headers = {
+          'Content-Type': 'application/json',
+          'x-tenant-name': 'coherent',
+          'x-synthetic-key': 'facaae76-30e7-4201-9cc7-683dd3a751c6'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        outputs = json.loads(response.text)['response_data']['outputs']
+        
+        return outputs
 
     def _calculate_put_option_price(self): 
         """
         Put option price calculation. Calculating payoffs for simulated prices at expiry date, summing up, averiging them and discounting.   
         Put option payoff (it's exercised only if the price at expiry date is lower than a strike price): max(K - S_t, 0)
         """
-        if self.simulation_results_S is None:
-            return -1
-        return np.exp(-self.r * self.T) * 1 / self.N * np.sum(np.maximum(self.K - self.simulation_results_S[-1], 0))
-       
+        
+        url = "https://excel.staging.coherent.global/coherent/api/v3/folders/Microsoft Envision/services/MonteCarloSimulation/Execute"
 
-    def plot_simulation_results(self, num_of_movements):
-        """Plots specified number of simulated price movements."""
-        plt.figure(figsize=(12,8))
-        plt.plot(self.simulation_results_S[:,0:num_of_movements])
-        plt.axhline(self.K, c='k', xmin=0, xmax=self.num_of_steps, label='Strike Price')
-        plt.xlim([0, self.num_of_steps])
-        plt.ylabel('Simulated price movements')
-        plt.xlabel('Days in future')
-        plt.title(f'First {num_of_movements}/{self.N} Random Price Movements')
-        plt.legend(loc='best')
-        plt.show()
+        payload = json.dumps({
+          "request_data": {
+            "inputs": {
+              "daystoexpire": self.num_of_steps,
+              "historicvolatility": self.sigma,
+              "numSimulations": self.N,
+              "price": self.S_0,
+              "riskfreerate": self.r,
+              "strikeprice": self.K
+            }
+          },
+          "request_meta": {
+            "version_id": "4d5274e8-9b0d-49f6-873e-536537b237be",
+            "call_purpose": "Spark - API Tester",
+            "source_system": "SPARK",
+            "correlation_id": "",
+            "requested_output": None,
+            "service_category": "",
+            "compiler_type": "Type3"
+          }
+        })
+        headers = {
+          'Content-Type': 'application/json',
+          'x-tenant-name': 'coherent',
+          'x-synthetic-key': 'facaae76-30e7-4201-9cc7-683dd3a751c6'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        outputs = json.loads(response.text)['response_data']['outputs']
+        
+        return outputs
